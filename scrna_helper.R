803b2633b0e23d3d5d2d137edc2d27debcb8805a
Brooke Wolford <bwolford@umich.edu>
2019-06-20 12:55:26 -0400

    Create scrna_helper.R

---
+++ scrna_helper.R
@@ -0,0 +1,9 @@
+#load data, note this is hg38
+load("/home/bdsi2019/genomics/data/scrna/Human_genes__GRCh38_p12_.rda")
+
+#list environmental variables
+ls()
+
+#preview the gene information
+head(gene.location)
+
