b6dc86cec57257185bb5af791295f8dc0fdb880c
Brooke Wolford <bwolford@umich.edu>
2019-06-21 16:35:33 -0400

    Delete scrna_helper.R

--- scrna_helper.R
+++
@@ -1,9 +0,0 @@
-#load data, note this is hg38
-load("/home/bdsi2019/genomics/data/scrna/Human_genes__GRCh38_p12_.rda")
-
-#list environmental variables
-ls()
-
-#preview the gene information
-head(gene.location)
-
